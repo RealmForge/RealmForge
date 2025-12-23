e6464c47a24ecaf5a4f9cd635eb9c1d2646fa385
Jadestar01 <jadestar0904@gmail.com>
2025-12-23 20:40:01 +0900

    Edit Bootstrap option

--- Assets/01. Scripts/Game/Network/NetcodeBootStrap.cs
+++ Assets/01. Scripts/Game/Network/NetcodeBootStrap.cs
@@ -8,7 +8,8 @@ public class NetCodeBootstrap : ClientServerBootstrap
 {
     public override bool Initialize(string defaultWorldName)
     {
-        AutoConnectPort = 7979;
+        // Relay 사용 시 자동 연결 비활성화 (수동으로 Listen/Connect 호출)
+        AutoConnectPort = 0;
         return base.Initialize(defaultWorldName);
     }
 }
